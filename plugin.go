4a057cbe353ab0c8e0a383e1adb5c67a194ef095
Yan Foto <yan.foto@quaintous.com>
2016-11-15 13:15:03 +0100

    Add actual auth logic / Fix spacing

--- plugin.go
+++ plugin.go
@@ -1,45 +1,86 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
+	"fmt"
+	"regexp"
+	"strings"
+	"io/ioutil"
 	"encoding/json"
+	"github.com/docker/go-plugins-helpers/authorization"
 )
 
 const (
-  rulesPath = "/etc/sesame/rules.json"
+	rulesPath = "/etc/sesame/rules.json"
 )
 
 // Rule defines an allowed action by defining method and path pattern of a
 // Docker Remote API.
 // see https://docs.docker.com/engine/reference/api/docker_remote_api/
 type Rule struct {
-  Method string   `json:"method"`
-  Pattern string  `json:"pattern"`
+	Method string   `json:"method"`
+	Pattern string  `json:"pattern"`
+}
+
+// matches checks if given method equals to Rule's method and if the given URI
+// matches the Rule's pattern
+func (r *Rule) matches(method string, uri string) bool {
+	matched := false
+
+	if strings.EqualFold(r.Method, method) {
+		// TODO take care of errors!
+		matched, _ = regexp.MatchString(r.Pattern, uri)
+	}
+
+	return matched
 }
 
 // sesame implements the Plugin inteface of Docker authorization API and manages
 // authorization usign RuleSets
 type sesame struct {
-  rules map[string][]Rule
+	rules map[string][]Rule
 }
 
 // newPlugin creates a new Sesame plugin.
 // It first loads the rules and then registers the unix socket.
 func newPlugin() (*sesame, error) {
-  var plugin sesame
+	var plugin sesame
 
-  // Read the rules and decode them
-  content, err := ioutil.ReadFile(rulesPath)
-  if err != nil {
-    return nil, err
-  }
+	// Read the rules and decode them
+	content, err := ioutil.ReadFile(rulesPath)
+	if err != nil {
+		return nil, err
+	}
 
-  err = json.Unmarshal(content, &plugin.rules)
-  if err != nil {
-    return nil, err
-  }
+	err = json.Unmarshal(content, &plugin.rules)
+	if err != nil {
+		return nil, err
+	}
 
-  // We're gut to go!
-  return &plugin, nil
+	// We're gut to go!
+	return &plugin, nil
+}
+
+func (p *sesame) AuthZReq(req authorization.Request) authorization.Response {
+	user := req.User
+
+	if rules, ok := p.rules[user]; ok {
+		method := req.RequestMethod
+		uri := req.RequestURI
+
+		for _, rule := range rules {
+			if rule.matches(method, uri) {
+				return authorization.Response{Allow: true}
+			}
+		}
+	}
+
+	return authorization.Response{
+		Allow: false,
+		Msg: fmt.Sprintf("User '%s' not found!", user),
+	}
+}
+
+func (p *sesame) AuthZRes(req authorization.Request) authorization.Response {
+	// Our decision is final!
+	return authorization.Response{Allow: true}
 }
